c6d34032b5f8e7ec61406fa2bad49b37e7a6577e
Muhammad Samiullah <msami398@gmail.com>
2024-03-01 00:34:31 +0500

    feature: Add socket.io client + write socketprovider + context + simple UI for chat application

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,12 +1,18 @@
 import http from "http";
+import SocketService from "./services/socket";
 
 const init = async () => {
   const httpServer = http.createServer();
+  const socketService = new SocketService();
   const PORT = process.env.PORT || 8000;
 
+  //   attaching socket service to http server
+  socketService.io.attach(httpServer);
   httpServer.listen(PORT, () => {
     console.log(`HTTP server started at PORT: ${PORT}`);
   });
+
+  socketService.initListeners();
 };
 
 init();
